2143d82c7ac5af1291820565270421985818ab52
Yaarraaa <ysale7986@gmail.com>
2024-02-08 18:39:18 +0200

    connect with mongo db

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const connection = require('./config/connection')
+require('dotenv').config()
 
 const app = express();
 
+
+app.use(express.json());
+connection()
+
 app.get('/', (req, res) => res.send({ status: 'Ok🤝' }));
 
+const PORT = process.env.PORT || 8000
 
-app.listen(3000, ()=> console.log(`app runing on prot 3000`))
+app.listen(PORT, ()=> console.log(`server is runing in ${process.env.NODE_ENV} mode on prot ${PORT}`))
