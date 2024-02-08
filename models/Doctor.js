46269b26d17bac7e342f477fdd42aff3b4544078
Yaarraaa <ysale7986@gmail.com>
2024-02-08 19:03:40 +0200

    create a doctor shema

---
+++ models/Doctor.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+
+// doctor schema
+
+const DoctorShema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    ID: {
+        type: String,
+        required: true,
+        maxlength: 14,
+    },
+    location: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    experience: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30
+    }
+}, {
+    timestamps: true
+})
+
+const Doctor = mongoose.model("Doctor", DoctorShema);
+
+module.exports = {
+    Doctor
+}
